3c19413984a3b41809a16be9daa069f6d04f50c4
Maggie Rose <maggie.rose.g@gmail.com>
2017-04-19 08:50:16 -0500

    Add support for Brewery Beers API

--- lib/brewerydb/brewery.rb
+++ lib/brewerydb/brewery.rb
@@ -2,5 +2,9 @@ module Brewerydb
   class Brewery < Base
     collection_path '/breweries'
     member_path '/brewery'
+    def self.beers(brewery_id, params = {})
+      response = http.get("#{@member_path}/#{brewery_id}/beers", params)
+      Beers.new(response['data'])
+    end
   end
 end
